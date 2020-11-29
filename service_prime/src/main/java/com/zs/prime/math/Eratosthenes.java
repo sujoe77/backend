f6a0a5cae3db9959269d269937cc1d5057880bd6
sujoe <sujoe77@gmail.com>
2020-11-29 23:11:28 +0100

    improve Eratosthenes.java, add unit tests

--- service_prime/src/main/java/com/zs/prime/math/Eratosthenes.java
+++ service_prime/src/main/java/com/zs/prime/math/Eratosthenes.java
@@ -1,22 +1,26 @@
 package com.zs.prime.math;
 
 public class Eratosthenes implements PrimeCalculator {
+
     public int[] getPrimes(int n) {
+        if (n <= 0) {
+            return new int[]{};
+        }
         int oneToN[] = new int[n + 1];
         int primeCount = n, prime = 2;
         while (prime * prime <= n) {
-            primeCount = removeMultiplies(oneToN, primeCount, prime);
+            primeCount -= removeMultiplies(oneToN, prime);
             prime = findNextPrime(oneToN, prime);
         }
-        return toArray(oneToN, primeCount);
+        return toPrimeNumbers(oneToN, primeCount);
     }
 
-    private int removeMultiplies(int[] oneToN, int primeCount, int prime) {
+    private int removeMultiplies(int[] oneToN, int prime) {
+        int primeCount = 0;
         for (int i = 2 * prime; i < oneToN.length; i += prime) {
             if (oneToN[i] == 0) {
                 oneToN[i] = 1;
-                primeCount--;
-//                System.out.println("remove " + i);
+                primeCount++;
             }
         }
         return primeCount;
@@ -25,19 +29,17 @@ public class Eratosthenes implements PrimeCalculator {
     private int findNextPrime(int[] oneToN, final int lastPrime) {
         for (int i = lastPrime + 1; (i * i) < oneToN.length; i++) {
             if (oneToN[i] == 0) {
-//                System.out.println("new Prime is: " + i);
                 return i;
             }
         }
         return oneToN.length;
     }
 
-    private int[] toArray(int[] oneToN, int primeCount) {
+    private int[] toPrimeNumbers(int[] oneToN, int primeCount) {
         int[] ret = new int[primeCount];
         int j = 0;
-        for (int i = 1; i < oneToN.length; i++) {
+        for (int i = 1; i < oneToN.length && j < primeCount; i++) {
             if (oneToN[i] == 0) {
-//                System.out.printf("%d ", i);
                 ret[j++] = i;
             }
         }
