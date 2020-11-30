5132445c10255d9abbd8d9f23423d765e11d44ef
sujoe <sujoe77@gmail.com>
2020-11-30 08:47:59 +0100

    add failSafe

--- service_prime/src/main/java/com/zs/prime/grpc/PrimeClient.java
+++ service_prime/src/main/java/com/zs/prime/grpc/PrimeClient.java
@@ -17,30 +17,71 @@
 package com.zs.prime.grpc;
 
 import io.grpc.ManagedChannel;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.FailsafeExecutor;
+import net.jodah.failsafe.RetryPolicy;
+import net.jodah.failsafe.Timeout;
 
-import java.util.List;
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 import static com.zs.prime.grpc.PrimeServiceGrpc.newBlockingStub;
 import static io.grpc.ManagedChannelBuilder.forAddress;
 import static java.lang.String.format;
 import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.SEVERE;
 
 public final class PrimeClient {
     private static final Logger logger = Logger.getLogger(PrimeClient.class.getName());
 
-    public PrimeResponse callGPPC(int n, String address, int port) {
+    public PrimeResponse callGPPC(int n, String address, int port) throws ExecutionException, InterruptedException {
         long start = System.currentTimeMillis();
         logger.log(INFO, format("Calling gRPC service: %s:%d with %d", address, port, n));
-        ManagedChannel channel = forAddress(address, port)
-                .usePlaintext()
-                .build();
+        ManagedChannel[] channel = new ManagedChannel[]{null};
+        PrimeResponse response;
+        try {
+            channel[0] = forAddress(address, port)
+                    .usePlaintext()
+                    .build();
 
-        PrimeRequest request = PrimeRequest.newBuilder().setN(n).build();
-        PrimeResponse response = newBlockingStub(channel).getPrimes(request);
-        channel.shutdown();
-        logger.log(INFO, format("gRPC service return: %s, errorCode: %d, errorMessage: [%s], latency: %d milli-seconds",
-                response.getPrimeList().toString(), response.getErrorCode(), response.getErrorMessage(), System.currentTimeMillis() - start));
+            PrimeRequest request = PrimeRequest.newBuilder()
+                    .setN(n)
+                    .build();
+            FailsafeExecutor<Object> executor = Failsafe.with(getRetryPolicy(), getTimeOut());
+            response = executor
+                    .getAsync(() -> newBlockingStub(channel[0]).getPrimes(request))
+                    .get();
+            if (response != null) {
+                logger.log(INFO, format("gRPC service return: %s, errorCode: %d, errorMessage: [%s], latency: %d milli-seconds",
+                        response.getPrimeList().toString(),
+                        response.getErrorCode(),
+                        response.getErrorMessage(),
+                        System.currentTimeMillis() - start));
+            }
+        } finally {
+            if (channel[0] != null) {
+                channel[0].shutdown();
+            }
+        }
         return response;
+    }
+
+    private RetryPolicy<Object> getRetryPolicy() {
+        RetryPolicy<Object> retryPolicy = new RetryPolicy<>()
+                .handle(Throwable.class)
+                .withDelay(Duration.ofSeconds(5))
+                .withMaxRetries(3)
+                .onRetriesExceeded(e -> {
+                    logger.log(SEVERE, format("Exception met when call gRPC service: %s", e.getFailure().getMessage()));
+                    throw e.getFailure();
+                });
+        return retryPolicy;
+    }
+
+    private Timeout<Object> getTimeOut() {
+        return Timeout.of(Duration.ofSeconds(30)).onFailure(event -> {
+            throw new RuntimeException("Send log to Azure timeout!!", event.getFailure());
+        });
     }
 }
--- service_prime/src/test/java/com/zs/prime/grpc/PrimeClientTest.java
+++ service_prime/src/test/java/com/zs/prime/grpc/PrimeClientTest.java
@@ -1,46 +1,112 @@
 package com.zs.prime.grpc;
 
-import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
 import mockit.Mocked;
+import net.jodah.failsafe.*;
+import net.jodah.failsafe.function.CheckedSupplier;
+import org.joor.Reflect;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static org.joor.Reflect.onClass;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class PrimeClientTest {
-    @Mocked
-    ManagedChannelBuilder builder;
     @Mocked
     PrimeResponse response;
     @Mocked
     PrimeServiceGrpc.PrimeServiceBlockingStub stub;
 
     @Test
-    public void testCallGPPC() {
-        new MockUp<ManagedChannelBuilder>() {
+    public void testCallGPPC() throws ExecutionException, InterruptedException {
+        final ManagedChannelBuilder[] builder = new ManagedChannelBuilder[]{null};
+        builder[0] = new MockUp<ManagedChannelBuilder>() {
             @Mock
             public ManagedChannelBuilder<?> forAddress(String name, int port) {
                 assertEquals(name, "localhost");
                 assertEquals(port, 8080);
-                return builder;
+                return builder[0];
             }
-        };
+
+            @Mock
+            public ManagedChannelBuilder usePlaintext() {
+                return builder[0];
+            }
+        }.getMockInstance();
         new MockUp<PrimeServiceGrpc>() {
             @Mock
             public PrimeServiceGrpc.PrimeServiceBlockingStub newBlockingStub(io.grpc.Channel channel) {
                 return stub;
             }
         };
+        List<PrimeRequest> requestList = new LinkedList<>();
+        new Expectations() {{
+            stub.getPrimes(withCapture(requestList));
+            result = response;
+        }};
 
-        new MockUp<PrimeServiceGrpc.PrimeServiceBlockingStub>() {
+        PrimeClient client = new PrimeClient();
+        PrimeResponse ret = client.callGPPC(100, "localhost", 8080);
+        assertEquals(ret, response);
+        assertEquals(requestList.get(0).getN(), 100);
+    }
+
+    @Test
+    public void testCallGPPCFailsafe() throws ExecutionException, InterruptedException {
+        final ManagedChannelBuilder[] builder = new ManagedChannelBuilder[]{null};
+        builder[0] = new MockUp<ManagedChannelBuilder>() {
             @Mock
-            public com.zs.prime.grpc.PrimeResponse getPrimes(com.zs.prime.grpc.PrimeRequest request) {
-                assertEquals(request.getN(), 100);
-                return response;
+            public ManagedChannelBuilder<?> forAddress(String name, int port) {
+                assertEquals(name, "localhost");
+                assertEquals(port, 8080);
+                return builder[0];
+            }
+
+            @Mock
+            public ManagedChannelBuilder usePlaintext() {
+                return builder[0];
+            }
+        }.getMockInstance();
+        new MockUp<PrimeServiceGrpc>() {
+            @Mock
+            public PrimeServiceGrpc.PrimeServiceBlockingStub newBlockingStub(io.grpc.Channel channel) {
+                return stub;
             }
         };
+        new MockUp<FailsafeExecutor>() {
+            @Mock
+            void $init(List<Policy> policies) {
+                assertEquals(policies.size(), 2);
+                assertTrue(policies.get(0) instanceof RetryPolicy);
+                assertEquals(((RetryPolicy<?>) policies.get(0)).getMaxRetries(), 3);
+                assertEquals(((RetryPolicy<?>) policies.get(0)).getDelay(), Duration.ofSeconds(5));
+                assertTrue(policies.get(1) instanceof Timeout);
+                assertEquals(((Timeout<?>) policies.get(1)).getTimeout(), Duration.ofSeconds(30));
+            }
+
+            @Mock
+            private <T> CompletableFuture<T> callAsync(Function<AsyncExecution, Supplier<CompletableFuture<ExecutionResult>>> supplierFn, boolean asyncExecution) {
+                return new CompletableFuture<T>(){
+                    @Override
+                    public T get() throws InterruptedException, ExecutionException {
+                        return (T) response;
+                    }
+                };
+            }
+        };
+
         PrimeClient client = new PrimeClient();
         PrimeResponse ret = client.callGPPC(100, "localhost", 8080);
         assertEquals(ret, response);
