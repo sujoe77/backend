77e0f2dd89a3857a16c9ef96073f18234e66e76f
sujoe77 <sujoe77@gmail.com>
2024-09-14 21:30:47 +0200

    more clean up

--- rust/ownership/src/tests/my_struct.rs
+++ rust/ownership/src/tests/my_struct.rs
@@ -1,3 +1,4 @@
+#[cfg(test)]
 #[test]
 fn test_area() {
     use crate::my_struct::Rectangle;
@@ -16,4 +17,26 @@ fn test_perimeter() {
         height: 8.0,
     };
     assert_eq!(dbg!(r.perimeter()), 24.0);
+}
+
+#[test]
+fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+}
+
+#[test]
+fn test_rectangle() {
+    use crate::my_struct::Rectangle;
+
+    let scale = 2.0;
+    let rect1 = Rectangle {
+        width: dbg!(30.0 * scale),
+        height: 50.0,
+    };
+
+    print!("{}", rect1.area());
+    print!("{}", Rectangle::area2(3.0, 15.0)); //call associated
+
+    dbg!(&rect1);
 }
---
+++ rust/ownership/src/tests/rust_ownership.rs
@@ -0,0 +1,43 @@
+#[test]
+fn test_take_share() {
+    use crate::rust_ownership::take_share;
+
+    let s = String::from("value");
+    let mut s_mutable = String::from("value2");
+    let sp = &String::from("value");
+    let sp_mutable = &mut String::from("value");
+
+    take_share(sp);
+    take_share(&s);
+    take_share(&s_mutable);
+    take_share(sp_mutable);
+
+    print!("{}", s);
+    print!("{}", s_mutable);
+    print!("{}", sp);
+    print!("{}", sp_mutable);
+}
+
+#[test]
+fn test_take() {
+    use crate::rust_ownership::take;
+
+    let s = String::from("value");
+    let mut mutable_s = String::from("value2");
+    take(s);
+    //println!("s is {}", s); //can not compile
+    take(mutable_s);
+}
+
+#[test]
+fn test_take_mutable() {
+    use crate::rust_ownership::take_mutable;
+
+    let _s = String::from("value");
+    let mut mutable_s = String::from("value2");
+
+    //takeMutable(&mut s); does not compile, s is immutable
+    take_mutable(&mut mutable_s);
+    println!("{}", mutable_s);
+    //takeMutable(&mut s); //not compile, s immutable
+}
