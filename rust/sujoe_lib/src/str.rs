66a6e623752c04dcff9c3948520a737036f2c730
zhou <sujoe77@gmail.com>
2024-09-22 13:37:24 +0200

    more clean up

--- rust/sujoe_lib/src/str.rs
+++ rust/sujoe_lib/src/str.rs
@@ -1,13 +1,13 @@
 use crate::math::add;
 use regex::Regex;
 
-pub fn regex_func() {
+pub fn regex_func(str: &str) {
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    println!("Did our date match? {}", re.is_match("2014-01-01"));
+    println!("Did our date {} match? {}", str, re.is_match(str));
 }
 
-pub fn show_text() {
-    println!("show_text is called!");
+pub fn show_text(str: &str) {
+    println!("{}", str);
 }
 
 pub fn show_maty() {
---
+++ rust/sujoe_lib/src/tests/str.rs
@@ -0,0 +1,11 @@
+use crate::str::*;
+
+#[test]
+fn test_regex_func() {
+    regex_func("2024-01-01");
+}
+
+#[test]
+fn test_show_text(){
+    show_text("show_text is called!");
+}
