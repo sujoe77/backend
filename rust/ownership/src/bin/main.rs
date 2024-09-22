1a471ba6407a4a37f8913f839781d367e1393aa3
sujoe77 <sujoe77@gmail.com>
2024-09-22 20:08:17 +0000

    more cleanup

--- rust/ownership/src/bin/main.rs
+++ rust/ownership/src/bin/main.rs
@@ -1,29 +1,3 @@
-use ownership::rust_ownership::*;
-
 fn main() {
     println!("Hello, world!");
-    use_after_assign();
-    clone_str();
-    copy_int();
-
-    let str = String::from("hello");
-    let str2 = takes_and_gives_back(str);
-    //print!("{}", str);  this failed
-    print!("{}", str2);
-
-    let str2_ref = &str2;
-    pass_ref(str2_ref);
-    pass_ref(&str2);
-    print!("{}", str2);
-
-    let mut s = String::from("str");
-    change(&mut s);
-    s.push_str("s");
-    let s1 = &mut s;
-    change(s1);
-    change(s1);
-
-    pass_ref(&s);
-
-    let _s_immutable = String::from("hi");
 }
--- rust/ownership/src/tests/rust_ownership.rs
+++ rust/ownership/src/tests/rust_ownership.rs
@@ -1,3 +1,5 @@
+use crate::rust_ownership::*;
+
 #[test]
 fn test_take_share() {
     use crate::rust_ownership::take_share;
@@ -40,4 +42,51 @@ fn test_take_mutable() {
     take_mutable(&mut mutable_s);
     println!("{}", mutable_s);
     //takeMutable(&mut s); //not compile, s immutable
+}
+
+#[test]
+fn test_change() {
+    let mut s = String::from("str");
+    change(&mut s);
+    s.push_str("s");
+    let s1 = &mut s;
+    change(s1);
+    change(s1);
+
+    pass_ref(&s);
+
+    let _s_immutable = String::from("hi");
+}
+
+#[test]
+fn test_pass_ref() {
+    let str = String::from("hello");
+    let str2 = str;
+    let str2_ref = &str2;
+    pass_ref(str2_ref);
+    pass_ref(&str2);
+    print!("{}", str2);
+}
+
+#[test]
+fn test_takes_and_gives_back() {
+    let str = String::from("hello");
+    let str2 = takes_and_gives_back(str);
+    //print!("{}", str);  this failed
+    print!("{}", str2);
+}
+
+#[test]
+fn test_copy_init() {
+    copy_int();
+}
+
+#[test]
+fn test_clone_str() {
+    clone_str();
+}
+
+#[test]
+pub fn test_use_after_assign() {
+    use_after_assign();
 }
