8aa5630353f1ec0f6adbebc1f7a8cfcd239bf5ee
sujoe77 <sujoe77@gmail.com>
2024-10-09 00:24:24 +0200

    fixed build

--- latency/chronicalMap/build.gradle.kts
+++ latency/chronicalMap/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     java
 }
 
-group="com.pineapple"
-version="1.0-SNAPSHOT"
+group = "com.pineapple"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -11,11 +11,18 @@ repositories {
 
 dependencies {
     implementation("net.openhft:chronicle-map:3.26ea4")
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.11.2")
-    testRuntimeOnly( "org.junit.jupiter:junit-jupiter-engine:5.11.2")    
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.2")
 }
 
 tasks.named<Test>("test") {
-    //jvmArgs("-Xmx1536M --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-exports=java.base/java.lang.reflect=ALL-UNNAMED")
+    jvmArgs(
+        "-Xmx1536M",
+        "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED",
+        "--add-exports=java.base/java.lang.reflect=ALL-UNNAMED",
+        "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
+        "--add-opens=java.base/java.lang=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+    )
     useJUnitPlatform()
 }
