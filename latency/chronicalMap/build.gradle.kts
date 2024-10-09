159c062985785811dbfc62fd62fcdfafb4bc3575
sujoe77 <sujoe77@gmail.com>
2024-10-09 20:01:48 +0200

    fix test jvmArgs

--- latency/chronicalMap/build.gradle.kts
+++ latency/chronicalMap/build.gradle.kts
@@ -23,7 +23,7 @@ tasks.named<Test>("test") {
         "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
         "--add-exports=java.base/java.lang.reflect=ALL-UNNAMED",
         "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-        "--add-exports java.base/jdk.internal.ref=ALL-UNNAMED"
+        "--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED"
     )
     useJUnitPlatform()
 }
