cc6ef36ac4a6d71a44782891d0d0f847ff5263d8
sujoe77 <sujoe77@gmail.com>
2024-10-09 19:10:33 +0200

    git pull in setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,6 @@
 ## update and install some things we should probably have
 mkdir /usr/local/sdkman/candidates/java
 ln -s /usr/lib/jvm/msopenjdk-current /usr/local/sdkman/candidates/java/current
+
+cd /workspaces/backend
+git pull
