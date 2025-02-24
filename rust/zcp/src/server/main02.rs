538df4266fab477af82f1fd1e2fff3fc14b44bc0
sujoe77 <sujoe77@gmail.com>
2025-02-24 15:33:35 +0000

    refactoring

--- rust/zcp/src/server/main02.rs
+++ rust/zcp/src/server/main02.rs
@@ -2,48 +2,53 @@ use chrono::Local;
 use std::{env, time::Duration};
 //use systemstat::{saturating_sub_bytes, Platform, System};
 use tokio::{
-  io::AsyncWriteExt,
-  net::{TcpListener, TcpStream},
+    io::AsyncWriteExt,
+    net::{TcpListener, TcpStream},
 };
 
 #[tokio::main]
 async fn main() {
     let mut port = String::from("4000");
     if let Some(_) = env::args().find(|f| f.starts_with("-port")) {
-      port = env::args().nth(2).unwrap_or(String::from("4000"));
-    } 
-  
-    println!("Application started");
-    
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
-  
-    println!("Waiting to clients... [Port: {}]", port);
-  
-    loop {
-      match listener.accept().await {
-        Ok((socket, _)) => {
-          tokio::spawn(async move {
-            process(socket).await;
-          });
-        }
-        Err(e) => println!("Error in acception: {}", e),
-      }
+        port = env::args().nth(2).unwrap_or(String::from("4000"));
     }
-  }
 
-  async fn process(mut socket: TcpStream) {
-    println!("Connected this client, Remote Addr: {:?}", socket.peer_addr().unwrap());
-    socket.set_nodelay(true).unwrap();
-  
+    println!("Application started");
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+
+    println!("Waiting to clients... [Port: {}]", port);
+
     loop {
-      std::thread::sleep(Duration::from_secs(1));
-  
-      let local = Local::now();
-      let time = local.format("%H:%M:%S\n").to_string();
-  
-      if let Err(e) = socket.write_all(time.as_bytes()).await {
-        println!("During send, socket has given error: {}", e);
-        return;
-      }
+        match listener.accept().await {
+            Ok((socket, _)) => {
+                tokio::spawn(async move {
+                    process(socket).await;
+                });
+            }
+            Err(e) => println!("Error in acception: {}", e),
+        }
     }
-  }
+}
+
+async fn process(mut socket: TcpStream) {
+    println!(
+        "Connected this client, Remote Addr: {:?}",
+        socket.peer_addr().unwrap()
+    );
+    socket.set_nodelay(true).unwrap();
+
+    loop {
+        std::thread::sleep(Duration::from_secs(1));
+
+        let local = Local::now();
+        let time = local.format("%H:%M:%S\n").to_string();
+
+        if let Err(e) = socket.write_all(time.as_bytes()).await {
+            println!("During send, socket has given error: {}", e);
+            return;
+        }
+    }
+}
