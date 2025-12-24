bb5db6b2cbef89d8c39df9e7d9087dc9aa95ed75
sujoe77 <sujoe77@gmail.com>
2025-12-24 12:49:28 +0100

    finished hello

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,14 +1,20 @@
 use std::{
     fs,
     io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream},    
 };
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
